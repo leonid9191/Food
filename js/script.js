a053d8baf029cc9f3d656b90179b6add7496ba49
leonid9191 <leonid0991@gmail.com>
2020-10-06 12:11:39 +0300

    server php

--- js/script.js
+++ js/script.js
@@ -147,66 +147,14 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     window.addEventListener('scroll', showModalByScroll);
 
-    //cards 
+    //Forms
 
-    class menuCards{
-        constructor (src, alt, title, discr,price, parentSelector){
-            this.src = src;
-            this.alt = alt;
-            this.title = title;
-            this.discr = discr;
-            this.usdToRub = 67;
-            this.price = price;
-            this.parent = document.querySelector(parentSelector);
-            this.changeUsdToRub();
-        }
+    const forms = document.querySelectorAll('form');
 
-        changeUsdToRub(){
-            this.price = this.price * this.usdToRub;
-        }
-
-        render(){
-            const element = document.createElement('div');
-            element.innerHTML = `
-            <div class="menu__item">
-                <img src=${this.src} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.title}</h3>
-                <div class="menu__item-descr">${this.discr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Price</div>
-                    <div class="menu__item-total"><span>${this.price}</span> rub/day</div>
-                </div>
-            </div>
-            `;
-            this.parent.append(element);
-        }
+    function postData(form){
+        form.addEventListener('submit', (e) => {
+            e.prevenDefault();//it's a function for don't reload page after sending form
+            
+        });
     }
-
-    new menuCards(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        10,
-        ".menu .container" 
-    ).render();
-
-    new menuCards(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        15,
-        ".menu .container" 
-    ).render();
-
-    new menuCards(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        19,
-        ".menu .container" 
-    ).render();
 });
